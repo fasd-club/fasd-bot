5877e00688f5b3d1f306eb53024c4bf3ae73983d
GG <gbgdev@gmail.com>
2017-11-21 23:24:50 -0200

    Migrating bot repo to organization

---
+++ app.js
@@ -0,0 +1,44 @@
+const TelegramBot = require('node-telegram-bot-api');
+const WundergroundApi = require('wunderground-api-client');
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
+
+var wunderContext = new WundergroundApi.WunderApi(process.env.WUNDERGROUND_TOKEN, {lang: 'EN'}, 'hourly', 'conditions');
+
+bot.onText(/\@weather$/, (msg, match) => {
+  wunderContext.query('zmw:00000.1.02485')
+    .then(function (result) {
+      const chatId = msg.chat.id;
+
+      var current_observation = result.current_observation;
+
+      var output = `Current temperature on Stockholm, Sweden\n`;
+      output += `<b>${current_observation.temp_c} °C</b> (Feels like ${current_observation.feelslike_c} °C)\n\n`;
+
+      for (data of result.hourly_forecast.slice(0,7)) {
+        var hour = data.FCTTIME.hour_padded + ':00';
+        var emoji = "\u{231A}";
+        output += `${emoji} ${hour} | Temp: ${data.temp.metric} °C | Precip: ${data.pop}%\n`
+      }
+
+      bot.sendMessage(chatId, output, { parse_mode: 'html'});
+    }).fail(function (err) {console.dir(err)}).done();
+
+});
+
+bot.onText(/heloisa/i, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "¯\\_(ツ)_/¯", { parse_mode: 'markdown'});
+});
+
+bot.onText(/janete/i, (msg, match) => {
+  const chatId = msg.chat.id;
+
+  var output = 'https://goo.gl/hdVn48';
+
+  bot.sendMessage(chatId, output, { parse_mode: 'markdown'});
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+});
