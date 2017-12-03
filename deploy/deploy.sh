f1198f09aadc49f24291ff5bccf28abf9c8e77db
GG <gbgdev@gmail.com>
2017-12-03 17:52:22 +0100

    Fix deploy commands order

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,9 +2,10 @@
 
 cd $FASD_BOT_DIR_PATH && git pull origin master
 
+docker ps -q -f "name=fasd-bot" | xargs --no-run-if-empty docker stop $1
+
 make cleandocker build
 
-docker ps -q -f "name=fasd-bot" | xargs --no-run-if-empty docker stop $1
 docker run --name fasd-bot --rm -d -v $(echo $FASD_BOT_DIR_PATH):/src -e TELEGRAM_TOKEN=$(echo $TELEGRAM_TOKEN) -e WUNDERGROUND_TOKEN=$(echo $WUNDERGROUND_TOKEN) -e NODE_ENV=$(echo $NODE_ENV) fasd-bot:latest
 
 npm install --prefix=./deploy
