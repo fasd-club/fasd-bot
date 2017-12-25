91b0a23dc380ca9d642b4b388523a94601e9e651
GG <gbgdev@gmail.com>
2017-12-25 17:40:56 +0100

    Update janete image

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ bot.onText(/\@weather$/, (msg, match) => {
 bot.onText(/janete/i, (msg, match) => {
   const chatId = msg.chat.id;
 
-  var output = 'https://goo.gl/VNco83';
+  var output = 'https://preview.ibb.co/k9niAR/jn.jpg';
 
   bot.sendMessage(chatId, output, { parse_mode: 'markdown'});
 });
