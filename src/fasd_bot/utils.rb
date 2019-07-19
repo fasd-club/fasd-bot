ec2c6085f65e0d6b52c4780e00601e8f7f828e10
Igor Belo <igorcoura@gmail.com>
2019-07-19 12:51:51 +0200

    fix wrong method name call

--- src/fasd_bot/utils.rb
+++ src/fasd_bot/utils.rb
@@ -7,7 +7,7 @@ module Utils
   end
 
   def self.resize_image(file, dimensions = '512x512', format = 'png')
-    image_url = get_image_url(file)
+    image_url = get_file_url(file)
     img = MiniMagick::Image.open(image_url)
     img.resize dimensions
     img.format format
