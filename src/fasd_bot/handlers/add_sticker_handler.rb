3dab3324c2321b35020b8c7d1ce85f295da58b40
Igor Belo <igorcoura@gmail.com>
2019-07-19 12:48:21 +0200

    fix missing file_id on add sticker handler

--- src/fasd_bot/handlers/add_sticker_handler.rb
+++ src/fasd_bot/handlers/add_sticker_handler.rb
@@ -3,30 +3,42 @@ require 'fasd_bot/utils'
 
 module AddStickerHandler
   def self.handle(bot, message)
-    file_id = get_file_id(message)
-    file = bot.api.get_file(file_id: file_id)
-    resized_image = Utils.resize_image(file)
+    resized_image =
+      get_file_id(message)
+        .then { |file_id| bot.api.get_file(file_id: file_id) }
+        .then { |file| Utils.resize_image(file) }
 
-    bot.api.add_sticker_to_set(
-      user_id: Config::BOT_OWNER_ID,
-      name: Config::BOT_NAME,
-      png_sticker: Faraday::UploadIO.new(resized_image.path, 'image/png'),
-      emojis: message.text.split(' ')[1]
-    )
+    emojis = message.text.split(' ')[1]
+    add_sticker_to_set(bot, resized_image, emojis)
 
-    bot.api.send_message(
-      chat_id: message.chat.id,
-      text: 'Sticker autárquico adicionado com sucesso.'
-    )
+    send_feedback(bot, message)
   end
 
   def self.handle?(message)
-    /^-fasdeia- .+$/i =~ message.text && (message.reply_to_message&.photo&.any? || message.reply_to_message&.sticker)
+    /^-fasdeia- .+$/i =~ message.text &&
+    (message.reply_to_message&.photo&.any? || message.reply_to_message&.sticker)
   end
 
   private
 
   def self.get_file_id(message)
-    message.reply_to_message&.photo || message.reply_to_message&.sticker
+    return message.reply_to_message.photo.max_by { |f| f.file_size }.file_id if message.reply_to_message&.photo&.any?
+    message.reply_to_message.sticker.file_id if message.reply_to_message&.sticker
+  end
+
+  def self.add_sticker_to_set(bot, resized_image, emojis)
+    bot.api.add_sticker_to_set(
+      user_id: Config::BOT_OWNER_ID,
+      name: Config::BOT_NAME,
+      png_sticker: Faraday::UploadIO.new(resized_image.path, 'image/png'),
+      emojis: emojis
+    )
+  end
+
+  def self.send_feedback(bot, message)
+    bot.api.send_message(
+      chat_id: message.chat.id,
+      text: 'Sticker autárquico adicionado com sucesso.'
+    )
   end
 end
