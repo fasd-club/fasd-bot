1d12ab07de93d52fb9808f7d10ea76aeaf1e5290
Igor Belo <igorcoura@gmail.com>
2020-03-27 17:17:41 +0100

    add sextas

--- src/fasd_bot/handlers/generic_reply_handler.rb
+++ src/fasd_bot/handlers/generic_reply_handler.rb
@@ -1,5 +1,3 @@
-require 'fasd_bot/config'
-
 module GenericReplyHandler
   def self.handle(bot, message)
     reply_message = get_reply_message(message)
@@ -20,30 +18,34 @@ module GenericReplyHandler
 
   def self.get_reply_message(message)
     case message.text
+    when /^-fasd- (sexta|sextas|sextou)/i
+      'https://i.redd.it/gnzgfzhqxy301.jpg'
     when /^-fasd- healthcheck$/i
       'Tô aqui carniça!'
     when /^-fasd- (terça|terças)$/i
       """
-      Ó terça, não sois a segunda,
-      Que de tédio abunda
-      Porém sois sua prima,
-      Que em sua jornada nos torna saudosistas,
-      Fazendo-nos lembrar de felizes momentos de outrora,
-      Onde cada indivíduo felicidade emana,
-      No também conhecido 'final de semana'
+      Ode às terças-feiras
+
+Ó terça, não sois a segunda,
+Que de tédio abunda
+Porém sois sua prima,
+Que em sua jornada nos torna saudosistas,
+Fazendo-nos lembrar de felizes momentos de outrora,
+Onde cada indivíduo felicidade emana,
+No também conhecido 'final de semana'
       """
     when /^-fasd- (quarta|quartas)$/i
       """
-      Ode às Quartas-feiras
+      Ode às quartas-feiras
 
-      Ó, quartas-feiras
-      Por que me judiastes?
-      Ó, quartas-feiras
-      Por que sois tão distantes
-      Tanto do final de semana passado
-      Quanto do que está por vir
-      Ó, quartas-feiras
-      Por que não passastes...
+Ó, quartas-feiras
+Por que me judiastes?
+Ó, quartas-feiras
+Por que sois tão distantes
+Tanto do final de semana passado
+Quanto do que está por vir
+Ó, quartas-feiras
+Por que não passastes...
       """
   end
 end
